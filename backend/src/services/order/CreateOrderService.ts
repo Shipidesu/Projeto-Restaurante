a4498109d0c9a87035829a5a1b974fe5bcb30c24
Vitor Lima <vitordlima17@gmail.com>
2023-03-02 17:26:46 -0300

    Atualização 3.0

--- backend/src/services/order/CreateOrderService.ts
+++ backend/src/services/order/CreateOrderService.ts
@@ -11,7 +11,7 @@ class CreateOrderService{
             data:{
                 table: table,
                 name: name,
-            }
+            }  
         })
         
         return order;
